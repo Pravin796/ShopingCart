2f380fdc9d880e80cd9b30e6188209f84916a60d
Pravin796 <pravinswain232@gmail.com>
2026-05-31 23:01:40 +0530

    created delete update api of carts and carts items

--- src/main/java/com/pravin/shopping_cart/controller/CartController.java
+++ src/main/java/com/pravin/shopping_cart/controller/CartController.java
@@ -3,17 +3,20 @@ package com.pravin.shopping_cart.controller;
 import com.pravin.shopping_cart.dto.AddItemToCartRequest;
 import com.pravin.shopping_cart.dto.CartDto;
 import com.pravin.shopping_cart.dto.CartItemDto;
+import com.pravin.shopping_cart.dto.UpdateCartItemRequest;
 import com.pravin.shopping_cart.entities.Cart;
 import com.pravin.shopping_cart.entities.CartItem;
 import com.pravin.shopping_cart.mappers.CartMapper;
 import com.pravin.shopping_cart.repositories.CartRepository;
 import com.pravin.shopping_cart.repositories.ProductRepository;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -42,7 +45,7 @@ public class CartController {
             @PathVariable UUID cartId,
             @RequestBody AddItemToCartRequest request
     ){
-        var cart = cartRepository.findById(cartId).orElse(null);
+        var cart = cartRepository.getCartWithItems(cartId).orElse(null);
         if(cart == null) {
             return ResponseEntity.notFound().build();
         }
@@ -53,20 +56,7 @@ public class CartController {
             return ResponseEntity.badRequest().build();
         }
 
-        var cartItem = cart.getCartItems().stream()
-                .filter(item -> item.getProduct().getId().equals(product.getId()))
-                .findFirst()
-                .orElse(null);
-
-        if(cartItem != null){
-            cartItem.setQuantity(cartItem.getQuantity() + 1);
-        }else{
-            cartItem = new CartItem();
-            cartItem.setProduct(product);
-            cartItem.setQuantity(1);
-            cartItem.setCart(cart);
-            cart.getCartItems().add(cartItem);
-        }
+        var cartItem =  cart.addItem(product);
 
         cartRepository.save(cart);
         var cartItemdto = cartMapper.toDto(cartItem);
@@ -74,13 +64,75 @@ public class CartController {
 
     }
 
-//    @GetMapping("/{cartId}")
-//    public ResponseEntity<CartDto> getCart(@PathVariable UUID cartId){
-//        var cart = cartRepository.findById(cartId).orElse(null);
-//        if(cart == null){
-//            return ResponseEntity.notFound().build();
-//        }
-//
-//        return ResponseEntity.ok(cartMapper.toDto(cart));
-//    }
+    @GetMapping("/{cartId}")
+    public ResponseEntity<CartDto> getCart(@PathVariable UUID cartId){
+        var cart = cartRepository.getCartWithItems(cartId).orElse(null);
+        if(cart == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(cartMapper.toDto(cart));
+    }
+
+    @PutMapping("/{cartId}/items/{productId}")
+    public ResponseEntity<?> updateItem(
+            @PathVariable("cartId") UUID cartId,
+            @PathVariable("productId") Long productId,
+            @Valid @RequestBody UpdateCartItemRequest request
+    ){
+        var cart = cartRepository.getCartWithItems(cartId).orElse(null);
+        if(cart == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    Map.of("error", "Cart not found")
+            );
+        }
+
+        var cartItem = cart.getItem(productId);
+        if(cartItem == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    Map.of("error", "Product was not found in the cart")
+            );
+        }
+
+        cartItem.setQuantity(request.getQuantity());
+        cartRepository.save(cart);
+
+        return ResponseEntity.ok(cartMapper.toDto(cartItem));
+    }
+
+    @DeleteMapping("/{cartId}/items/{productId}")
+    public ResponseEntity<?> removeItem(
+            @PathVariable("cartId") UUID cartId,
+            @PathVariable("productId") Long productId
+    ){
+        var cart = cartRepository.getCartWithItems(cartId).orElse(null);
+        if(cart == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    Map.of("error", "Cart was not found")
+            );
+        }
+
+        cart.removeItem(productId);
+        cartRepository.save(cart);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{cartId}/items")
+    public ResponseEntity<Void> clearCart(
+            @PathVariable UUID cartId
+    ){
+        var cart = cartRepository.getCartWithItems(cartId).orElse(null);
+
+        if(cart == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        cart.clear();
+
+        cartRepository.save(cart);
+
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/com/pravin/shopping_cart/entities/Cart.java
+++ src/main/java/com/pravin/shopping_cart/entities/Cart.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -23,7 +24,54 @@ public class Cart {
     @Column(name = "date_created", insertable = false, updatable = false)
     private LocalDate dateCreated;
 
-    @OneToMany(mappedBy = "cart", cascade = CascadeType.MERGE)
-    private Set<CartItem> cartItems = new LinkedHashSet<>();
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.MERGE,orphanRemoval = true, fetch = FetchType.EAGER)
+    private Set<CartItem> items = new LinkedHashSet<>();
+
+    public BigDecimal getTotalPrice(){
+        BigDecimal total = BigDecimal.ZERO;
+        for(CartItem item : items){
+             total = total.add(item.getToalPrice());
+        }
+        return total;
+
+//        same but in advance
+//        return items.stream()
+//                .map(CartItem::getToalPrice)
+//                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    public CartItem getItem(Long product){
+        return items.stream()
+                .filter(items -> items.getProduct().getId().equals(product))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public CartItem addItem(Product product) {
+        var cartItem = getItem(product.getId());
+
+        if(cartItem != null){
+            cartItem.setQuantity(cartItem.getQuantity() + 1);
+        }else{
+            cartItem = new CartItem();
+            cartItem.setProduct(product);
+            cartItem.setQuantity(1);
+            cartItem.setCart(this);
+            items.add(cartItem);
+        }
+        return cartItem;
+    }
+
+    public void removeItem(Long productId){
+        var cartItem = getItem(productId);
+        if(cartItem != null){
+            items.remove(cartItem);
+            cartItem.setCart(null);
+        }
+    }
+
+    public void clear(){
+        items.clear();
+    }
 
 }
